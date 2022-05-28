18b2282f9843970c070fef07bedf61994b084e36
Ujjwal <ugautam@hawaii.edu>
2022-05-27 15:55:49 -1000

    initial commit testing new mongo db

--- Server/RouteFunctions/routeFunctions.js
+++ Server/RouteFunctions/routeFunctions.js
@@ -3,12 +3,14 @@ import MySchema from '../Models/listofevents.js';
 
 const router = express.Router();
 
-router.get('/', (req, res) => {
-    const getData = MySchema.find();
+router.get('/', async (req, res) => {
+    const myModel = new MySchema({name: "Ujjwal", tile: "NOC Tech", description:"Ujjwal is a bad boy", venue:"USA", people: ["Ram", "Shyam"]});
+    const result = async() => await myModel.save();
+    const getData = await MySchema.find();
     res.json(getData);
 });
 router.post('/', (req, res) => {
-    const myModel = new MySchema({name: "Ujjwal", tile: "NOC TEch", description:"Ujjwal is a bad boy", venue:"USA", people: ["Ram", "Shyam"]});
+    const myModel = new MySchema({name: "Ujjwal", tile: "NOC Tech", description:"Ujjwal is a bad boy", venue:"USA", people: ["Ram", "Shyam"]});
     myModel.save();
     // res.json(myModel.find());
 });
