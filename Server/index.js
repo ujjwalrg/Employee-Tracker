8dbaa33fb93a9ff1394e1c70901e6a021e63d9a9
Ujjwal <ugautam@hawaii.edu>
2022-08-14 09:11:13 -1000

    installed new dependencies on the server

--- Server/index.js
+++ Server/index.js
@@ -3,21 +3,20 @@ import mongoose from "mongoose";
 import cors from "cors";
 import getRoutes from "./Routes/routes.js";
 import bodyParser from "body-parser";
+import "dotenv/config";
 
+console.log(process.env);
 const server = express();
-const PORT = 3001;
+const PORT = process.env.PORT || 3001;
 
 // only look at the request with content type json
 server.use(bodyParser.json({ limit: "50mb" }));
 //accept only utf-8 encoding on the body
 server.use(bodyParser.urlencoded({ extended: true }));
-// server.use(bodyParser());
 
 server.use(cors());
 server.use("/", getRoutes);
-const db_URL =
-  "mongodb+srv://ujjwalrg:ujjwalrg123@life-recap.42fgm.mongodb.net/test?retryWrites=true&w=majority";
-const connection = async () => await mongoose.connect(db_URL);
+const connection = async () => await mongoose.connect(process.env.Mongo_URL);
 
 connection()
   .then(
